f07e235d1f861f738978e479850756b9648418da
arlohol <arlohol@cdw.com>
2021-04-27 20:32:32 -0400

    Adding lab_2.py and test_lab_2.py

---
+++ Tests/test_lab_2.py
@@ -0,0 +1,8 @@
+import unittest
+from Labs import lab_2
+
+
+class TestModule2(unittest.TestCase):
+    """
+    Series of tests to validate Lab 2 on basic flow control operations and loops
+    """
